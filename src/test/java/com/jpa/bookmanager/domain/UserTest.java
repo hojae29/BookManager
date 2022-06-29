4cb69a370cd3c3bed9d906d1a36db983dd3ae33b
hojae29 <gkqthd6386@naver.com>
2022-06-29 23:07:36 +0900

    Lombok

---
+++ src/test/java/com/jpa/bookmanager/domain/UserTest.java
@@ -0,0 +1,25 @@
+package com.jpa.bookmanager.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+class UserTest {
+
+    @Test
+    void test(){
+        User user = new User();
+        user.setEmail("gkqthd6386@naver.com");
+        user.setName("hojae");
+
+        User user1 = new User("hojae", "gkqthd6386@naver.com", LocalDateTime.now(), LocalDateTime.now());
+        User user2 = new User("hojae", "gkqthd6386@naver.com");
+
+        User.builder()
+                .name("hojae")
+                .email("gkqthd6386@naver.com")
+                .build();
+
+        System.out.println(">>> " + user);
+    }
+}
