80bebac62e6f6d15e9056e47d2719723d1ffaa1b
hojae29 <gkqthd6386@naver.com>
2022-07-02 20:41:01 +0900

    Jpa Pagination

--- src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -5,11 +5,8 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Sort;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -19,8 +16,15 @@ class UserRepositoryTest {
 
     @Test
     void crud(){
-        userRepository.saveAndFlush(new User("new hojae", "newhojae@naver.com"));
+        Page<User> users = userRepository.findAll(PageRequest.of(0, 3));
 
-        userRepository.findAll().forEach(System.out::println);
+        System.out.println("page : " + users);
+        System.out.println("totalElements : " + users.getTotalElements());
+        System.out.println("totalPages : " + users.getTotalPages());
+        System.out.println("numberOfElements : " + users.getNumberOfElements());
+        System.out.println("sort : " + users.getSort());
+        System.out.println("size : " + users.getSize());
+
+        users.getContent().forEach(System.out::println);
     }
 }
